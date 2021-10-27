33b61e05fe2350df8ee1f3acd82d2864da5329b3
Paul Schweigert <paulschw@us.ibm.com>
2021-10-27 16:47:26 -0400

    Cluster tests (#142)
    * scripts to test serving/eventing working
    
    * copyright
    
    * simplifying scripts
    
    * typo

---
+++ test/getting-started-test.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# Copyright 2021 The Knative Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -euo pipefail
+
+# Serving Test
+echo "creating ksvc"
+kn service create hello --image gcr.io/knative-samples/helloworld-go --port 8080 --env TARGET=World --revision-name=world
+SERVICE=$(kn service describe hello -o url)
+
+echo "curling ksvc 'hello'"
+curl "$SERVICE"
+
+# Eventing Tests
+kn broker list
+echo "creating cloudevents player"
+kn service create cloudevents-player --image ruromero/cloudevents-player:latest --env BROKER_URL=http://broker-ingress.knative-eventing.svc.cluster.local/default/example-broker
+PLAYER=$(kn service describe cloudevents-player -o url)
+
+echo "posting event"
+curl -v "$PLAYER"   -H "Content-Type: application/json"   -H "Ce-Id: foo-1"   -H "Ce-Specversion: 1.0"   -H "Ce-Type: dev.example.events"   -H "Ce-Source: curl-source"   -d '{"msg":"Hello team!"}'
+
+echo "creating trigger"
+kn trigger create cloudevents-trigger --sink cloudevents-player  --broker example-broker
+
+echo "posting trigger event"
+curl -v "$PLAYER"   -H "Content-Type: application/json"   -H "Ce-Id: foo-1"   -H "Ce-Specversion: 1.0"   -H "Ce-Type: dev.example.trigger"   -H "Ce-Source: curl-source"   -d '{"msg":"Hello team!"}'
+
+echo "minikube test finished"
